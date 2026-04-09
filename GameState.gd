c331f41718cb6d2718593ec5cbfb5293005fc1df
Blueracey <bromlingc@gmail.com>
2026-04-08 19:14:45 -0600

    Missed a comment in game state

--- GameState.gd
+++ GameState.gd
@@ -1,12 +1,11 @@
 extends TileMapLayer
 
+#global variable which contains map tile information 
 var mapData = []
-# Called when the node enters the scene tree for the first time.
 
 
-func _ready():
-	print("Game state loaded")
-	
+
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
